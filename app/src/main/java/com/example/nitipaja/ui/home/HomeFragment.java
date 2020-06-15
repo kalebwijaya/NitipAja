2381e26fd0f6112e4fcd39ddda98d358f45de21d
Raiman <wijayakaleb@gmail.com>
2020-06-15 18:17:49 +0700

    Home Fragment Add List New Produk Done

--- app/src/main/java/com/example/nitipaja/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/nitipaja/ui/home/HomeFragment.java
@@ -15,20 +15,38 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.nitipaja.R;
+import com.example.nitipaja.ui.category.CategoryActivity;
+import com.example.nitipaja.ui.category.CategoryAdapter;
+import com.example.nitipaja.ui.category.CategoryModel;
 import com.example.nitipaja.ui.home.orderProduct.OrderProductActivity;
 import com.example.nitipaja.ui.search.SearchActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    private String userID;
+
     private List<Category> categoryList;
     private Button btnToOrder;
     private EditText mSearchBar;
+    private RecyclerView rvCategory, rvListProduk;
+
+    private FirebaseAuth fAuth;
+    private DatabaseReference databaseReference;
+    private ArrayList<CategoryModel> listProduct;
+    private CategoryAdapter categoryAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -44,6 +62,16 @@ public class HomeFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
         mSearchBar = root.findViewById(R.id.et_home_search_bar);
+        rvCategory = root.findViewById(R.id.rv_category);
+        rvListProduk = root.findViewById(R.id.rv_home_produk_list);
+
+        fAuth = FirebaseAuth.getInstance();
+        userID = fAuth.getUid();
+        listProduct = new ArrayList<>();
+        rvListProduk.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("transaction");
+
         mSearchBar.setOnEditorActionListener(editorListener);
 
         btnToOrder = root.findViewById(R.id.btn_home_to_order_item);
@@ -56,10 +84,31 @@ public class HomeFragment extends Fragment {
             }
         });
 
-        RecyclerView rvCategory = root.findViewById(R.id.rv_category);
+
         CategoryRecyclerViewAdapter myAdapter = new CategoryRecyclerViewAdapter(getContext(), categoryList);
         rvCategory.setLayoutManager(new GridLayoutManager(getContext(),4));
         rvCategory.setAdapter(myAdapter);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+                    CategoryModel categoryModel = postSnapshot.getValue(CategoryModel.class);
+                    if(!(categoryModel.getUserID().equals(userID))
+                            && (categoryModel.getItemStatus().equals("Menunggu"))
+                    ){
+                        listProduct.add(categoryModel);
+                    }
+                }
+                categoryAdapter = new CategoryAdapter(getContext(),listProduct);
+                rvListProduk.setAdapter(categoryAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         return root;
     }
